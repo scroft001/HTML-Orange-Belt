4b297b4dd1c853b73112c3dba114780c4e271469
Steven Croft <scroft001@gmail.com>
2022-01-06 10:58:51 -0800

    Added project 2

--- project2.js
+++ project2.js
@@ -15,11 +15,35 @@ if (ans1 === 'Squirtle' || ans1 == 'squirtle') {
 let ans2 = prompt('What year did Minecraft come out?');
 
 if (ans2 === '2011') {
-	money += 100;
-	alert('Correct! you won 100 dollars!');
+	money += 200;
+	alert('Correct! you won 200 dollars!');
 	alert('You now have ' + money + ' dollars!');
 } else {
 	money -= 100;
 	alert('Wrong! You lost 100 dollars!');
 	alert('You now have ' + money + ' dollars!');
+}
+
+let ans3 = prompt('In Marvel Comics and movies, who is known as the Mad Titan?');
+
+if (ans3 === 'Thanos' || ans3 == 'thanos') {
+	money += 500;
+	alert('Correct! you won 500 dollars!');
+	alert('You now have ' + money + ' dollars!');
+} else {
+	money -= 100;
+	alert('Wrong! You lost 100 dollars!');
+	alert('You now have ' + money + ' dollars!');
+}
+
+alert('You have won ' + money + " dollars. Let's see what prize you are taking home!");
+
+if (money >= 800) {
+	location.href = 'car.html';
+} else if (money >= 300) {
+	location.href = 'iphone.html';
+} else if (money >= 0) {
+	location.href = 'gum.html';
+} else {
+	location.href = 'kanga.html';
 }
